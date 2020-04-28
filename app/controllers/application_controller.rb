fe71900acf8b8b8b76681bf21f8e7c7db3935c2a
aditiagrahari-18 <aditi@akaruilabs.com>
2020-04-28 13:52:12 +0530

    Update User Profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
+  def logged_in_user
+      unless user_signed_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
   protected
 
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-   devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-    user_params.permit(:username, :email)
-   end
+   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :password, :password_confirmation, :email, :dob])
+
   end
 end
