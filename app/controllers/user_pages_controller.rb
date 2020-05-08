d84573d0cf7f7af625a19aac26b1334be0f40267
aditiagrahari-18 <aditi@akaruilabs.com>
2020-05-08 17:22:05 +0530

    complted friend list

--- app/controllers/user_pages_controller.rb
+++ app/controllers/user_pages_controller.rb
@@ -1,5 +1,8 @@
 class UserPagesController < ApplicationController
   def home
-    
+    users = User.where.not(id: current_user)
+    @friend = Invite.where(status: 'accepted', invited_by: current_user)
+    @friends = users.where(id: @friend.pluck(:invited_to))
+  
   end
 end
