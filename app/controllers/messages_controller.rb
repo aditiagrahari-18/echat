ca224a170cbb6ef9c0d0c54d17f644befc5aad10
aditiagrahari-18 <aditi@akaruilabs.com>
2020-05-15 13:00:18 +0530

    completed user archived

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
-
   def create
+    @user = User.where(id: params[:sender_id]).or(User.where(id: params[:reciever_id]))
+    if @user.where(archived: true)
+    @user.update(archived: false)
     @message = Message.new(user_params)
     @message.sender_id = params[:sender_id]
     @message.reciever_id = params[:reciever_id]
@@ -8,6 +10,16 @@ class MessagesController < ApplicationController
     respond_to do |format|
     format.html { redirect_to root_path }
     format.js
+    end
+  else
+    @message = Message.new(user_params)
+    @message.sender_id = params[:sender_id]
+    @message.reciever_id = params[:reciever_id]
+    @message.save
+      respond_to do |format|
+          format.html { redirect_to root_path }
+          format.js
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'users/index'
   devise_for :users
   root 'user_pages#home'
+  post 'user_pages/block'
   get 'users/invites'
   delete 'users/destroy'
   get 'users/accept_request'
